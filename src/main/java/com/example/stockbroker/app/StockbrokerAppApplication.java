39a633bcad34801c8f7a8849d786f565ec044d66
saahithhegde <saahithhegde@yahoo.co.in>
2019-11-01 12:21:35 -0500

    Initial commit spring template

---
+++ src/main/java/com/example/stockbroker/app/StockbrokerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockbroker.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockbrokerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockbrokerAppApplication.class, args);
+	}
+
+}
